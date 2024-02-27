f94af2219b18492bc69e54a9e9fee8a7a0d2fa20
supplyandcommand <42962106+supplyandcommand@users.noreply.github.com>
2024-02-27 16:23:12 +0100

    Initialize repo

---
+++ fetch_datasets.sh
@@ -0,0 +1,14 @@
+"""
+"""
+
+# data goes to the raw folder
+cd data/raw/
+
+# initialize git lfs
+git lfs install
+
+# genius lyrics on HF
+git clone https://huggingface.co/datasets/brunokreiner/genius-lyrics
+
+# genius lyrics on Kaggle downloaded manually from
+# https://www.kaggle.com/datasets/carlosgdcj/genius-song-lyrics-with-language-information?resource=download
